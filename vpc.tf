a804a27150803991de54dc92496ad3e35d4411ab
Yuki Okuno <y.okuno.btm@gmail.com>
2023-03-03 10:00:15 +0900

    route tableの定義を追加

--- vpc.tf
+++ vpc.tf
@@ -42,10 +42,20 @@ resource "aws_subnet" "sbcntr-subnet-private-container-1c" {
 # --------------------------------------------------------------
 # Route Table
 # --------------------------------------------------------------
-resource "aws_route_table" "sbcntr-route-app" {
+resource "aws_route_table" "sbcntr-route-table" {
     vpc_id = "${aws_vpc.sbcntr-vpc.id}"
     tags = {
         Project = var.project
-        Name = "sbcntr-route-app"
+        Name = "${var.project}-route-app"
     }
+}
+
+resource "aws_route_table_association" "sbcntr-route-table_1a" {
+    route_table_id = aws_route_table.sbcntr-route-table.id
+    subnet_id = aws_subnet.sbcntr-subnet-private-container-1a.id
+}
+
+resource "aws_route_table_association" "sbcntr-route-table_1c" {
+    route_table_id = aws_route_table.sbcntr-route-table.id
+    subnet_id = aws_subnet.sbcntr-subnet-private-container-1c.id
 }
