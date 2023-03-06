3f6bc96300b6db4ea6dc6120d83e4f9b0371c8a9
Yuki Okuno <y.okuno.btm@gmail.com>
2023-03-06 09:49:51 +0900

    db用のsbunet、route-tableを追加

--- vpc.tf
+++ vpc.tf
@@ -12,6 +12,8 @@ resource "aws_vpc" "sbcntr-vpc" {
     }
 }
 
+# アプリ用コンテナの設定
+
 # --------------------------------------------------------------
 # Subnet
 # --------------------------------------------------------------
@@ -40,7 +42,7 @@ resource "aws_subnet" "sbcntr-subnet-private-container-1c" {
 }
 
 # --------------------------------------------------------------
-# Route Table
+# Route Table（アプリ用コンテナ）
 # --------------------------------------------------------------
 resource "aws_route_table" "sbcntr-route-table" {
     vpc_id = "${aws_vpc.sbcntr-vpc.id}"
@@ -58,4 +60,53 @@ resource "aws_route_table_association" "sbcntr-route-table_1a" {
 resource "aws_route_table_association" "sbcntr-route-table_1c" {
     route_table_id = aws_route_table.sbcntr-route-table.id
     subnet_id = aws_subnet.sbcntr-subnet-private-container-1c.id
+}
+
+# DB周りの設定
+# --------------------------------------------------------------
+# Subnet
+# --------------------------------------------------------------
+resource "aws_subnet" "sbcntr-subnet-private-db-1a" {
+    vpc_id = "${aws_vpc.sbcntr-vpc.id}"
+
+    availability_zone = "ap-northeast-1a"
+    cidr_block = "10.0.16.0/24"
+    map_public_ip_on_launch = false
+
+    tags = {
+        Name = "sbcntr-subnet-private-db-1a"
+    }
+}
+
+resource "aws_subnet" "sbcntr-subnet-private-db-1c" {
+    vpc_id = "${aws_vpc.sbcntr-vpc.id}"
+
+    availability_zone = "ap-northeast-1c"
+    cidr_block = "10.0.17.0/24"
+    map_public_ip_on_launch = false
+
+    tags = {
+        Name = "sbcntr-subnet-private-db-1c"
+    }
+}
+
+# --------------------------------------------------------------
+# Route Table（DB用）
+# --------------------------------------------------------------
+resource "aws_route_table" "sbcntr-route-db" {
+    vpc_id = "${aws_vpc.sbcntr-vpc.id}"
+    tags = {
+        Project = var.project
+        Name = "${var.project}-route-db"
+    }
+}
+
+resource "aws_route_table_association" "sbcntr-route-db-1a" {
+    route_table_id = aws_route_table.sbcntr-route-db.id
+    subnet_id = aws_subnet.sbcntr-subnet-private-db-1a.id
+}
+
+resource "aws_route_table_association" "sbcntr-route-db-1c" {
+    route_table_id = aws_route_table.sbcntr-route-db.id
+    subnet_id = aws_subnet.sbcntr-subnet-private-db-1c.id
 }
